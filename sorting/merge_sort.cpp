83a8414325d2f56439e50b219ab292233d1ba3e3
James Milo <james.milo@rezdy.com>
2020-02-26 13:12:45 +1100

    Added basis for merge_sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> merge_sort(std::vector<int> arr)
+// std::vector<int> merge_sort(std::vector<int> arr)
+// {
+//     // step 1 - break a vector up into two halves and return the first half - Nice!
+//     std::vector<int> left_half(arr.begin(), arr.begin() + arr.size()/2);
+//     std::vector<int> right_half(arr.begin() + arr.size()/2, arr.begin() + arr.size());
+//     return right_half;
+// }
+
+void merge_sort(std::vector<int> arr, int l, int r)
 {
-    // step 1 - break a vector up into two halves and return the first half - Nice!
-    std::vector<int> left_half(arr.begin(), arr.begin() + arr.size()/2);
-    std::vector<int> right_half(arr.begin() + arr.size()/2, arr.begin() + arr.size());
-    return right_half;
+    
 }
 
 
 int main()
 {
     std::vector<int> vec = {125, 68, 23, 56, 400};
-    std::cout << "The unsorted vector contains the following elements: ";
-    for(int i = 0; i < vec.size(); i++)
-    {
-        std::cout << vec[i] << "  ";
-    }
-    std::cout << std::endl;
+    // std::cout << "The unsorted vector contains the following elements: ";
+    // for(int i = 0; i < vec.size(); i++)
+    // {
+    //     std::cout << vec[i] << "  ";
+    // }
+    // std::cout << std::endl;
     
-    std::vector<int> result = merge_sort(vec);
+    merge_sort(vec, 1, 2);
 
-    std::cout << "After sorting the result is: ";
-    for(int i = 0; i < result.size(); i++)
-    {
-        std::cout << result[i] << "  ";
-    }
-    std::cout << std::endl;
+    // std::cout << "After sorting the result is: ";
+    // for(int i = 0; i < result.size(); i++)
+    // {
+    //     std::cout << result[i] << "  ";
+    // }
+    // std::cout << std::endl;
 }
